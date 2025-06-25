8c995ffc24b558f5cf6db693efde184ae725caf1
Narwhalsss360 <franklynvasquez24@gmail.com>
2025-06-24 23:14:14 -0400

    Implemented encode-with-size and collect

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const Allocator = std.mem.Allocator;
 const File = std.fs.File;
 const OpenFlags = File.OpenFlags;
 const OpenMode = File.OPenMode;
-const Error = File.ReadError || File.WriteError || File.OpenError || Allocator.Error;
+const Error = File.ReadError || File.WriteError || File.OpenError || Allocator.Error || nstreamcom.CollectorError || nstreamcom.DecoderError;
 const CommandFunctionPointer = *const fn (Allocator, File) Error!void;
 const StaticStringMap = std.static_string_map.StaticStringMap;
 
@@ -29,6 +29,14 @@ const command_map = StaticStringMap(CommandFunctionPointer).initComptime(.{
     .{
         "decode",
         decodeCommand
+    },
+    .{
+        "encode-with-size",
+        encodeWithSizeCommand
+    },
+    .{
+        "collect",
+        collectCommand
     }
 });
 
@@ -90,4 +98,44 @@ fn encodeCommand(allocator: Allocator, file: File) Error!void {
     nstreamcom.encode(text, encoded);
 
     _ = try stdout.write(encoded);
+}
+
+fn encodeWithSizeCommand(allocator: Allocator, file: File) Error!void {
+    const stdout = std.io.getStdOut().writer();
+
+    const text = try file.readToEndAlloc(allocator, max_io_size);
+    defer allocator.free(text);
+
+    const encoded: []u8 = try allocator.alloc(u8, nstreamcom.asCollectedSize(@intCast(text.len)));
+    defer allocator.free(encoded);
+    nstreamcom.encodeWithSize(text, encoded);
+
+    _ = try stdout.write(encoded);
+}
+
+fn collectCommand(allocator: Allocator, file: File) Error!void {
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
+
+    const text = try file.readToEndAlloc(allocator, max_io_size);
+    defer allocator.free(text);
+
+    const decoded_buffer: []u8 = try allocator.alloc(u8, nstreamcom.asDataSize(@intCast(text.len)));
+    defer allocator.free(decoded_buffer);
+    var decoder = nstreamcom.Decoder { .buffer = decoded_buffer };
+    var collector = nstreamcom.Collector { .decoder = &decoder };
+
+    for (text) |byte| {
+        try collector.collect(byte);
+        if (collector.state == nstreamcom.CollectorState.Collected) {
+            break;
+        }
+    }
+
+    if (collector.state != nstreamcom.CollectorState.Collected) {
+        try stderr.print("Did not collect, current state: {s}", .{@tagName(collector.state)});
+        return;
+    }
+
+    _ = try stdout.write(decoded_buffer[0..collector.next_size]);
 }
